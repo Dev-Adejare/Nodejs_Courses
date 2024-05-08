2c5b1f2077b5e31491022856ab9e7a8a0c51e5e4
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-05-08 12:14:35 +0100

    update

--- Lesson-4/server.js
+++ Lesson-4/server.js
@@ -11,6 +11,21 @@ const myEmitter = new Emitter();
 
 const PORT = process.env.PORT || 3500;
 
+const serverFile = async ( filepath, contentType, response) => {
+
+try {
+    const data = await fsPromises.readFile(filepath, 'utf8');
+    response.writeHead(200, {'content-Type':contentType});
+    response.end(data);
+    
+} catch (error) {
+    console.log(error);
+    res.statusCode = 500;
+    res.end();
+    
+}
+}
+
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
 
@@ -26,18 +41,51 @@ const server = http.createServer((req, res) => {
       contentType = "text/javascript";
       break;
     case ".json":
-      contentType = "text/json";
+      contentType = "application/json";
       break;
     case ".jpg":
-      contentType = "text/jpeg";
+      contentType = "image/jpeg";
+      break;
+    case ".png":
+      contentType = "image/png";
       break;
     case ".txt":
       contentType = "text/plain";
       break;
-      default:
-         contentType = 'text/html'
+    default:
+      contentType = "text/html";
   }
-});
+
+  let filepath;
+
+  if (contentType === "text/html" && req.url === "/") {
+    filepath = path.join(__dirname, "views", "index.html");
+  } else if (contentType === "text/html" && req.url.slice(-1) === "/") {
+    filepath = path.join(__dirname, "views", req.url);
+  } else if (contentType === "text/html") {
+    filepath = path.join(__dirname, "views", req.url);
+  } else {
+    filepath = path.join(__dirname, req.url);
+  }
+
+  if(!extension && req.url.slice(-1) !== '/') filepath += '.html';
+
+  const fileExists = fs.existsSync(filepath)
+
+  if(fileExists) {
+
+  }else {
+    switch(path.parse(filepath).base) {
+        case 'old-page.html':
+            res.writeHead(301, { 'location': '/new-page.html' });
+            break
+            case 'www-page.html':
+            res.writeHead(301, { 'location': '/' });
+            res.end();
+            break
+            default:
+  }
+}});
 
 server.listen(PORT, () => console.log(`server running on port ${PORT}`));
 

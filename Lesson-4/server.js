8f5f20242014e67d76507c0156d01d42e85cb4f4
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-05-08 11:06:07 +0100

    update

--- Lesson-4/server.js
+++ Lesson-4/server.js
@@ -9,16 +9,37 @@ class Emitter extends EventEmitter {}
 
 const myEmitter = new Emitter();
 
-
 const PORT = process.env.PORT || 3500;
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
-})
+  console.log(req.url, req.method);
 
+  const extension = path.extname(req.url);
 
-server.listen(PORT, () => console.log(`server running on port ${PORT}`))
+  let contentType;
 
+  switch (extension) {
+    case ".css":
+      contentType = "text/css";
+      break;
+    case ".js":
+      contentType = "text/javascript";
+      break;
+    case ".json":
+      contentType = "text/json";
+      break;
+    case ".jpg":
+      contentType = "text/jpeg";
+      break;
+    case ".txt":
+      contentType = "text/plain";
+      break;
+      default:
+         contentType = 'text/html'
+  }
+});
+
+server.listen(PORT, () => console.log(`server running on port ${PORT}`));
 
 // myEmitter.on("log", (msg) => logEvents(msg));
 

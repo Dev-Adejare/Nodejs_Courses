0d88739264d72cabc4684f00103d6b010eb7c3aa
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-05-09 10:35:10 +0100

    update

--- Lesson-5/server.js
+++ Lesson-5/server.js
@@ -8,9 +8,11 @@ const path = require("path");
 const PORT = process.env.PORT || 3500;
 
 //serving static files>>>>>>>>Static files are like image, txt, and css>>>>>>>>
-app.use(express.static(path.join(__dirname)))
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.get()
+app.get("^/$|/index(.html)?", (req, res) => {
+  res.sendFile("./views/index.html", {root: __dirname})
+})
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
 

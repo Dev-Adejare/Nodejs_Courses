61e518b2f5ed73b77580fcc4406f7c74c086d3d5
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-05-09 11:10:17 +0100

    update

--- Lesson-5/server.js
+++ Lesson-5/server.js
@@ -11,10 +11,22 @@ const PORT = process.env.PORT || 3500;
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get("^/$|/index(.html)?", (req, res) => {
-  res.sendFile("./views/index.html", {root: __dirname})
+
+  // res.sendFile("./views/index.html", {root: __dirname})
+  res.sendFile(path.join(__dirname, 'views', "index.html"))
+})
+app.get("/new-page(.html)?", (req, res) => {
+
+  // res.sendFile(path.join(__dirname, 'views', "index.html"))
+res.sendFile(path.join(__dirname, 'views', "new-page.html"))
+})
+
+app.get("/old-page(.html)?", (req, res) => {
+  res.redirect(301, "new-page.html")
 })
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+
 
 
 

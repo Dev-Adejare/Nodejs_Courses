8e2b8ec7555333f27137440933a96c1505413050
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-05-09 12:00:40 +0100

    update

--- Lesson-5/server.js
+++ Lesson-5/server.js
@@ -1,59 +1,61 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const path = require("path");
-
-
-
 
 const PORT = process.env.PORT || 3500;
 
 //serving static files>>>>>>>>Static files are like image, txt, and css>>>>>>>>
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("^/$|/index(.html)?", (req, res) => {
-
   // res.sendFile("./views/index.html", {root: __dirname})
-  res.sendFile(path.join(__dirname, 'views', "index.html"))
-})
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
 app.get("/new-page(.html)?", (req, res) => {
-
   // res.sendFile(path.join(__dirname, 'views', "index.html"))
-res.sendFile(path.join(__dirname, 'views', "new-page.html"))
-})
+  res.sendFile(path.join(__dirname, "views", "new-page.html"));
+});
 
 app.get("/old-page(.html)?", (req, res) => {
-  res.redirect(301, "new-page.html")
-})
-
+  res.redirect(301, "new-page.html");
+});
 
 // ROUTE HANDLERS
-app.get("/hello(.html)?", (req, res, next) => {
-  console.log("You're trying to access hello.html")
-  next();                                         //Next is been used to send our response after we've input hello.
-},
-(req, res) => {
-  res.send("Hello Kenny, Aderayo and Hidee!!")
-}
+app.get(
+  "/hello(.html)?",
+  (req, res, next) => {
+    console.log("You're trying to access hello.html");
+    next(); //Next is been used to send our response after we've input hello.
+  },
+  (req, res) => {
+    res.send("Hello Kenny, Aderayo and Hidee!!");
+  }
 );
 
 const one = (req, res, next) => {
   console.log("one");
   next();
-}
+};
 const two = (req, res, next) => {
   console.log("two");
   next();
-}
+};
 const three = (req, res, next) => {
   console.log("three");
   res.send("Finished!!!");
-}
+};
 
-app.get("/chain(.html)?", [one, two, three])
+app.get("/chain(.html)?", [one, two, three]);
+
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404! Not Found" });
+  } else {
+    res.type("txt").send("404! Not Found");
+  }
+});
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
-
-
-
-
-

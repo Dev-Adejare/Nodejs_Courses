c3de0543a2032da6267ddfde5970bb6d1cc3098b
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-05-13 13:00:05 +0100

    update

--- express_routers-main/routes/api/employee.js
+++ express_routers-main/routes/api/employee.js
@@ -15,5 +15,42 @@ router
     .get((req, res) => {
         res.json(data.employees);
     })
+    .post((req, res) => {
+        res.json({
+            id: req.body.id,
+            firstname: req.body.firstname,
+            lastname: req.body.lastname,
+            role: "Tutor"
+        });
+    })
+    .put((req, res) => {
+        res.json({
+            id: req.body.id,
+            firstname: req.body.firstname,
+            lastname: req.body.lastname,
+            role: req.body.role
+        });
+    })
+    .delete((req, res) => {
+        res.json({
+            id: req.body.id
+        });
+    })
+
+    router.route('/:id').get((req, res) => {
+        // res.json({id: req.params.id});
+        const id = parseInt(req.params.id);
+        const item = data.employees.find(item => item.id === id)
+
+        if (!item) {
+            res.status(404).json({
+                'Message': 'Not Found'
+            })
+        } else {
+            res.json(item)
+        }
+    
+    })
 
 module.exports = router;
+           

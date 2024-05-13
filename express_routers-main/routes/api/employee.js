b2eeb9811d9ef8eb3ef8d6f9ba191ea6a5d4eef0
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-05-13 11:53:10 +0100

    update

--- express_routers-main/routes/api/employee.js
+++ express_routers-main/routes/api/employee.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const data = {};
+data.employees = require('../../data/employees.json');
 
 const path = require("path");
 
-router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "..", "data", "employees.json"));
-});
+//first-method
+// router.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "..", "..", "data", "employees.json"));
+// });
+
+router
+    .route("/")
+    .get((req, res) => {
+        res.json(data.employees);
+    })
 
 module.exports = router;

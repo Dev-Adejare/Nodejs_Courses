e2e1c54d8f236a2bd6a2fc72a582fbc6e3fa04aa
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-05-14 10:32:13 +0100

    update

---
+++ authentication/controllers/registerController.js
@@ -0,0 +1,11 @@
+const usersDB = {
+    users: require('./model/users.json'),
+    setUsers: function (data) {
+        this.users = data
+    }
+}
+
+const fsPromises = require('fs').promises
+const path = require('path');
+const bcrypt = require('bcrypt');
+

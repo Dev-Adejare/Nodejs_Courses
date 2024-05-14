d65ddf79813130e07536db9c5488ea1a826145f0
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-05-14 10:37:01 +0100

    update

--- authentication/controllers/registerController.js
+++ authentication/controllers/registerController.js
@@ -9,3 +9,17 @@ const fsPromises = require('fs').promises
 const path = require('path');
 const bcrypt = require('bcrypt');
 
+const handleNewUser = async ( req, res) => {
+    const {user, pwd} = req.body;
+
+    if (!user || !pwd) return res.status(400).json({"message": 'Usernsame and password are required'})
+
+        const duplicate = usersDB.users.find(person => person.username === user)
+        if (duplicate) return res.status(409) //meaning conflict
+
+        try {
+            
+        } catch (error) {
+            
+        }
+}

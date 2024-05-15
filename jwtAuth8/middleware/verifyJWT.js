54cb94abf5191ffcdb9a98d1accf1c5a24830f1d
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-05-15 10:26:33 +0100

    update

--- jwtAuth8/middleware/verifyJWT.js
+++ jwtAuth8/middleware/verifyJWT.js
@@ -1,10 +1,18 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
+const verifyJWT = (req, res, nextDay) => {
+  const authHeader = req.headers["authorization"];
+  if (!authHeader) return res.sendStatus(401);
+  console.log(authHeader); //bearer token
+  const token = authHeader.split(" ")[1]; // split method of the String class is used to divide a string into an array of substrings.
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) return res.sendStatus(403); //invalid token
+    req.user = decoded.username;
+    next();
+  });
+};
 
-const verifyJWT = (req, res, nextDay) => { 
-    const authHeader = req.headers["authorization"]
-    if(!authHeader) return res.sendStatus(401);
-    console.log(authHeader);   //bearer token
-    const token = authHeader.split(" ")[1];  // split method of the String class is used to divide a string into an array of substrings.
-}
+module.exports = {
+  verifyJWT,
+};

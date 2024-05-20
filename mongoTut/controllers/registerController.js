73e180ec2ca91a50f3cb9d0b8b77e10c483673b2
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-05-20 16:01:02 +0100

    update

--- mongoTut/controllers/registerController.js
+++ mongoTut/controllers/registerController.js
@@ -1,12 +1,17 @@
-const usersDB = {
-  users: require("../model/users.json"),
-  setUsers: function (data) {
-    this.users = data;
-  },
-};
+const User = require("../model/User"); // replace the code below
 
-const fsPromises = require("fs").promises;
-const path = require("path");
+// const usersDB = {
+//   users: require("../model/users.json"),
+//   setUsers: function (data) {
+//     this.users = data;
+//   },
+// };
+
+// const fsPromises = require("fs").promises;
+// const path = require("path");
+
+//commented code above no more needed
+
 const bcrypt = require("bcrypt");
 
 const handleNewUser = async (req, res) => {
@@ -17,21 +22,36 @@ const handleNewUser = async (req, res) => {
       .status(400)
       .json({ message: "Username and password are required" });
 
-  const duplicate = usersDB.users.find((person) => person.username === user);   // Check for duplicate username
+  const duplicate = await User.findOne({ username: user }).exec(); //replace the duplicate code below
+
+  // const duplicate = usersDB.users.find((person) => person.username === user);   // Check for duplicate username
   if (duplicate) return res.status(409); //meaning conflict
 
   try {
     // encrypting the password
-    const hashedPwd = await bcrypt.hash(pwd, 10);      // The number 10 indicates the cost factor, which determines how computationally intensive the hashing process is. Higher values increase the security but also require more processing time.
+    const hashedPwd = await bcrypt.hash(pwd, 10); // The number 10 indicates the cost factor, which determines how computationally intensive the hashing process is. Higher values increase the security but also require more processing time.
+
+    //create and store new user
+    const result = await User.create({
+      "username": user,
+      "password": hashedPwd
+    })
 
     //storing the new user
-    const newUser = { "username": user, "roles":{"User": 2001}, "password": hashedPwd };
-    usersDB.setUsers([...usersDB.users, newUser]);      // Add new user
-    await fsPromises.writeFile(
-      path.join(__dirname, "..", "model", "users.json"),
-      JSON.stringify(usersDB.users)
-    );
-    console.log(usersDB.users);
+    // const newUser = {
+    //   username: user,
+    //   roles: { User: 2001 },
+    //   password: hashedPwd,
+    // };
+
+    // usersDB.setUsers([...usersDB.users, newUser]); // Add new user
+    // await fsPromises.writeFile(
+    //   path.join(__dirname, "..", "model", "users.json"),
+    //   JSON.stringify(usersDB.users)
+    // );
+    // console.log(usersDB.users);
+
+
     res.status(201).json({ success: `New User ${user} created` });
   } catch (error) {
     res.status(500).json({ message: error.message });

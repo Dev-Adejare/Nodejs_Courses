e385bcf253db154a41778b3727c2a81410528b6e
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-05-21 10:23:35 +0100

    update

--- mongoTut/controllers/refreshTokenController.js
+++ mongoTut/controllers/refreshTokenController.js
@@ -10,14 +10,15 @@
 const User = require("../model/User");
 const jwt = require("jsonwebtoken");
 
-
-const handleRefreshToken =  async (req, res) => {
+const handleRefreshToken = async (req, res) => {
   const cookies = req.cookies;
   if (!cookies?.jwt) return res.sendStatus(401); // Unauthorized if no token provided
   const refreshToken = cookies.jwt;
-  const foundUser = usersDB.users.find(
-    (person) => person.refreshToken === refreshToken
-  ); //The code finds a user in the database whose refresh token matches the provided token.
+  const foundUser = await User.findOne({ refreshToken }).exec();
+
+  // const foundUser = usersDB.users.find(
+  //   (person) => person.refreshToken === refreshToken
+  // ); //The code finds a user in the database whose refresh token matches the provided token.
 
   if (!foundUser) return res.sendStatus(403); // Forbidden if no token provided
 
@@ -31,7 +32,7 @@ const handleRefreshToken =  async (req, res) => {
       return res.sendStatus(403);
 
     const accessToken = jwt.sign(
-      { "UserInfo": { "username": decoded.username, "roles": roles } },
+      { UserInfo: { username: decoded.username, roles: roles } },
 
       process.env.ACCESS_TOKEN_SECRET,
       { expiresIn: "60s" }

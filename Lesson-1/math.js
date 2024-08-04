d92301b5110efe483b2e7f90583c4938d0b5640a
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-08-04 16:00:00 +0100

    update

--- Lesson-1/math.js
+++ Lesson-1/math.js
@@ -1,6 +1,6 @@
 // const add = (a, b) => a + b;
-const div = (a, b) => a / b;
-// const mult = (a, b) => a * b;
+// const div = (a, b) => a / b;
+const mult = (a, b) => a * b;
 // const sub= (a, b) => a - b;
 
 // module.exports = {add, div, mult, sub};

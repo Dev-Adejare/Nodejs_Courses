8dd4fffc7bdda4527d614864c0d2d76adec8dc88
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-08-08 16:00:00 +0100

    update

--- Lesson-1/server.js
+++ Lesson-1/server.js
@@ -1,9 +1,11 @@
+// console.log("Hello Nodejs!");
+// console.log("Welcome here!");
+
+
 
 // const os = require("os");
 
-// console.log(os.type());
-// console.log(os.version());
-// console.log(os.homedir());
+
 
 
 

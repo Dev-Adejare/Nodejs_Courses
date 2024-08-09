851ddcb5aa392347d13cd21722c3ea42aabaa97c
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-08-09 16:00:00 +0100

    update

--- Lesson-1/server.js
+++ Lesson-1/server.js
@@ -5,13 +5,12 @@
 
 // const os = require("os");
 
+// console.log(os.type());
+// console.log(os.version());
+// console.log(os.homedir());
 
 
 
-
-
-// console.log(__dirname);
-// console.log(__filename);
 
 
 // const path = require('path');

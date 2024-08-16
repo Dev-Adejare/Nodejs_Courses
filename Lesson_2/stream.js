d3952891f2f627309fe044f2b0177bc7c4b8bb3f
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-08-16 16:00:00 +0100

    update

--- Lesson_2/stream.js
+++ Lesson_2/stream.js
@@ -1,4 +1,3 @@
-//Readable Streams: Readable streams allow you to read data from a source, such as a file, into your Node.js application.
 //Writable Streams: Writable streams allow you to write data from your Node.js application to a destination, such as a file.
 //pipe() method to copy data from one file to another(readable file to writeable file)
 

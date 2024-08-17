cc7d4613d0762dec403dcd1becdc0a6c3d41030e
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-08-17 16:00:00 +0100

    update

--- Lesson_2/stream.js
+++ Lesson_2/stream.js
@@ -1,4 +1,3 @@
-//Writable Streams: Writable streams allow you to write data from your Node.js application to a destination, such as a file.
 //pipe() method to copy data from one file to another(readable file to writeable file)
 
 const fs = require('fs');
